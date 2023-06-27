294ec458433ed84b06ddb50e85d693f3242873d9
Poison159 <siyabongahdb@gmail.com>
2023-06-27 22:03:51 +0200

    Added endpoint for magickImage resize and covert method

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-
 namespace ApiTest.Controllers;
+using ImageMagick;
 
 [ApiController]
 [Route("[controller]")]
@@ -22,18 +22,48 @@ public class WeatherForecastController : ControllerBase
     [Route("GetToken")]
     public string GetToken([FromBody] User user)
     {
-      return Helper.GenerateToken(user);
+        return Helper.GenerateToken(user);
     }
 
     [HttpPost(Name = "ValidateToken")]
     [Route("ValidateToken")]
     public int? ValidateToken([FromBody] Token token)
     {
-      return Helper.ValidateToken(token.TokenString);
+        return Helper.ValidateToken(token.TokenString);
     }
+
+    [HttpPost(Name="resize")]
+    [Route("resize")]
+    public IActionResult ResizeImage([FromForm] FileUploadModel model)
+    {
+        if (model.ImageFile == null || model.ImageFile.Length == 0)
+            return BadRequest("Image file is required.");
+
+        // Read the uploaded image file into a byte array
+        byte[] imageData;
+        using (var memoryStream = new MemoryStream())
+        {
+            model.ImageFile.CopyTo(memoryStream);
+            imageData = memoryStream.ToArray();
+        }
+        using (var image = new MagickImage(imageData))
+        {
+          int width = 1000;
+          int height = 1000;
+          image.Resize(width, height);
+          image.Format = MagickFormat.Jpeg;
+          byte[] resizedImageData = image.ToByteArray();
+          return File(resizedImageData, "image/jpeg");
+        }
+    }
+}
+
+public class FileUploadModel
+{
+  public IFormFile ImageFile { get; set; }
 }
 
 public class Token
 {
-     public string TokenString { get; set; } = string.Empty;
+    public string TokenString { get; set; } = string.Empty;
 }
